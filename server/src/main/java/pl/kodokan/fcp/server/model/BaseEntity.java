8360a2003e6d8e7a2f585f9316f425b54cdf94db
Michal Rzepka <michal00rzepka@gmail.com>
2021-05-10 19:11:15 +0200

    Create base entity

---
+++ server/src/main/java/pl/kodokan/fcp/server/model/BaseEntity.java
@@ -0,0 +1,19 @@
+package pl.kodokan.fcp.server.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Getter
+    @Setter
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
