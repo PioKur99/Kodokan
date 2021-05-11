e0023006efb67c5fed6420f00c8c5dc56917a746
Michal Rzepka <michal00rzepka@gmail.com>
2021-05-11 21:49:11 +0200

    Add role entity

---
+++ server/src/main/java/pl/kodokan/fcp/server/employee/model/Role.java
@@ -0,0 +1,9 @@
+package pl.kodokan.fcp.server.employee.model;
+
+import pl.kodokan.fcp.server.common.model.BaseEntity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Role extends BaseEntity {
+}
