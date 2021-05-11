085a63616ed01c8971c6922a2474979305ed3544
Michal Rzepka <michal00rzepka@gmail.com>
2021-05-11 21:51:58 +0200

    Add Family entity

---
+++ server/src/main/java/pl/kodokan/fcp/server/customer/model/Family.java
@@ -0,0 +1,9 @@
+package pl.kodokan.fcp.server.customer.model;
+
+import pl.kodokan.fcp.server.common.model.BaseEntity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Family extends BaseEntity {
+}
