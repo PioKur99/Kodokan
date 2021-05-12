518056036f755627b5b6ecb8a96d11458f0c01ea
Michal Rzepka <michal00rzepka@gmail.com>
2021-05-12 19:58:41 +0200

    Supplement TimeSheet primivites

--- server/src/main/java/pl/kodokan/fcp/server/employee/model/TimeSheet.java
+++ server/src/main/java/pl/kodokan/fcp/server/employee/model/TimeSheet.java
@@ -1,12 +1,22 @@
 package pl.kodokan.fcp.server.employee.model;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import pl.kodokan.fcp.server.common.model.BaseEntity;
 
 import javax.persistence.Entity;
+import java.time.LocalDateTime;
 
 @Entity
-@Getter @Setter
+@Getter @Setter @NoArgsConstructor
 public class TimeSheet extends BaseEntity {
+
+    private LocalDateTime startDateTime;
+    private LocalDateTime endDateTime;
+
+    public TimeSheet(LocalDateTime startDateTime, LocalDateTime endDateTime) {
+        this.startDateTime = startDateTime;
+        this.endDateTime = endDateTime;
+    }
 }
