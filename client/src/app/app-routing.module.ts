24761cdf696109b3fe7436200df241ff0dd37762
Kamil Jabloński <k.jablonski98@gmail.com>
2021-05-18 14:47:22 +0000

    poprawka konfliktu

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { ErrorRoutingModule } from './error-routing/error-routing.module';
 export const routes: Routes = [
   { path: '', redirectTo: '', pathMatch: 'full' },
   { path: 'error', component: UncaughtErrorComponent },
+  { path: 'receptionist-panel', loadChildren: () => import('./receptionist-panel/receptionist-panel.module').then(m => m.ReceptionistPanelModule) },
   { path: 'customer-panel', loadChildren: () => import('./customer-panel/customer-panel.module').then(m => m.CustomerPanelModule) },
   { path: '**', component: PageNotFoundComponent } // must always be last
 ];
