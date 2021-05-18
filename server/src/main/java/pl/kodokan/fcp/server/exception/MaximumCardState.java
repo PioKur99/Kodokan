6a4609161328aa48499f11d60f4094d5532b619e
Grzegorz Paleta <grzetan@gmail.com>
2021-05-18 18:56:02 +0000

    KODO-T-43-card-state-paleta Fixed bugs
    Deleted some endpoints and enriched customer entity
    
    Resolved issues after code review
    
    Removed useless imports
    
    Removed useless imports
    
    Merge remote-tracking branch 'origin/main' into KODO-T-43-card-state-paleta
    
    Finishing touches
    
    Simplified service logic and added card state DTO
    
    Added the rest of endpoints
    
    Added 3 request handlers for changing card state
    
    KODO-T-44 Added:
    Request that sets the discipline field to the customer.
    Request returns customer discipline.
    Request returns possible discipline names.
    
    
    Co-authored-by: kamil7108 <kamimac209@student.polsl.pl>

---
+++ server/src/main/java/pl/kodokan/fcp/server/exception/MaximumCardState.java
@@ -0,0 +1,4 @@
+package pl.kodokan.fcp.server.exception;
+
+public class MaximumCardState extends RuntimeException{
+}
---
+++ server/src/main/java/pl/kodokan/fcp/server/repository/CustomerRepository.java
@@ -0,0 +1,16 @@
+package pl.kodokan.fcp.server.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import pl.kodokan.fcp.server.model.CardState;
+import pl.kodokan.fcp.server.model.Customer;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer,Long> {
+    Optional<Customer> findById(Long id);
+
+    List<Customer> findAllByCardState(CardState state);
+}
