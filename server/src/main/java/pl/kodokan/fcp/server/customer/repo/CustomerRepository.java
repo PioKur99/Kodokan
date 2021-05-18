24e1708bb940744108ff6e8028b6b079d49432e7
grzetan <grzetan@gmail.com>
2021-05-18 21:24:21 +0200

    Fixed bug that caused doubled return objects

--- server/src/main/java/pl/kodokan/fcp/server/customer/repo/CustomerRepository.java
+++ server/src/main/java/pl/kodokan/fcp/server/customer/repo/CustomerRepository.java
@@ -6,14 +6,13 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.kodokan.fcp.server.customer.dto.FilteredCustomersDTO;
 import pl.kodokan.fcp.server.customer.model.Customer;
-import pl.kodokan.fcp.server.user.model.UserData;
 
 import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-    @Query("SELECT new pl.kodokan.fcp.server.customer.dto.FilteredCustomersDTO(u.id,ud.firstName,ud.lastName) FROM Customer u, UserData ud WHERE ud.firstName LIKE %:firstName% AND ud.lastName LIKE %:lastName% AND CAST(u.cardId as text) LIKE %:cardID% AND CAST(ud.phone as text) LIKE %:phone%")
+    @Query("SELECT new pl.kodokan.fcp.server.customer.dto.FilteredCustomersDTO(u.id,ud.firstName,ud.lastName) FROM Customer u LEFT JOIN UserData ud ON u.id=ud.id WHERE ud.firstName LIKE %:firstName% AND ud.lastName LIKE %:lastName% AND CAST(u.cardId as text) LIKE %:cardID% AND CAST(ud.phone as text) LIKE %:phone%")
     List<FilteredCustomersDTO> getCustomers(@Param("firstName") String firstName, @Param("lastName") String lastName, @Param("cardID") String cardID, @Param("phone") String phone);
 
 }
