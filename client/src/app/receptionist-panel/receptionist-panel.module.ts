ec6c921e681c310a02e7eae81bfcad2258bf3eda
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2021-05-18 20:39:47 +0200

    layout finished

--- client/src/app/receptionist-panel/receptionist-panel.module.ts
+++ client/src/app/receptionist-panel/receptionist-panel.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import { ReceptionistPanelRoutingModule } from './receptionist-panel-routing.module';
 import { ReceptionistPanelComponent } from './receptionist-panel.component';
 import {
-    IgxRippleModule,
-    IgxButtonModule,
-    IgxBottomNavModule,
-    IgxIconModule,
-    IgxDialogModule
+	IgxRippleModule,
+	IgxButtonModule,
+	IgxBottomNavModule,
+	IgxIconModule,
+	IgxDialogModule,
+	IgxInputGroupModule,
+	IgxToggleModule,
+	IgxDropDownModule,
+	IgxCheckboxModule
 } from 'igniteui-angular';
 import { CustomersComponent } from './customers/customers.component';
 import { EntranceComponent } from './entrance/entrance.component';
@@ -16,7 +20,7 @@ import { PassesComponent } from './passes/passes.component';
 import { AddAClientComponent } from './add-a-client/add-a-client.component';
 import { EntryRegistrationComponent } from './entry-registration/entry-registration.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
     declarations: [
         ReceptionistPanelComponent,
@@ -35,7 +39,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         IgxBottomNavModule,
         IgxIconModule,
         //BrowserAnimationsModule,
-        IgxDialogModule
+        IgxDialogModule,
+        FormsModule,
+        IgxInputGroupModule,
+        //BrowserAnimationsModule,
+        IgxToggleModule,
+        IgxDropDownModule,
+        IgxCheckboxModule
     ]
 })
 export class ReceptionistPanelModule {
