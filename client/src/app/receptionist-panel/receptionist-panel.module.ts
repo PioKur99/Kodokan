ce0dfa9cfd499b3c06a288129b1fb2ada1a50ff1
PioKur99 <kurdapiotr99@icloud.com>
2021-05-19 21:23:56 +0200

    dialog

--- client/src/app/receptionist-panel/receptionist-panel.module.ts
+++ client/src/app/receptionist-panel/receptionist-panel.module.ts
@@ -39,7 +39,7 @@ import { FormsModule } from '@angular/forms';
         IgxButtonModule,
         IgxBottomNavModule,
         IgxIconModule,
-        IgxDialogModule,
+        IgxDialogModule
     ]
 })
 export class ReceptionistPanelModule {
