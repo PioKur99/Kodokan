6ad06298dce3235e1a9e4f8aabe124e722456745
Grizzly <44000679+JablonskiKJ@users.noreply.github.com>
2021-05-20 20:57:23 +0200

    todos

--- client/src/app/receptionist-panel/entry-registration/entry-registration.component.ts
+++ client/src/app/receptionist-panel/entry-registration/entry-registration.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Training } from 'src/app/data/training/training';
 
 @Component({
@@ -10,20 +11,36 @@ export class EntryRegistrationComponent implements OnInit {
 
   trainingList: Training[]
   //__trainingList: Training[]
-  cosiek: Boolean
+  //TODO: dodać User do data
+  //user: User
+  userSub: Subscription
+  trainingSub: Subscription
+
+  @ViewChild("alertok") alertok;
+  @ViewChild("alertnotok") alertnotok;
+
   constructor() { 
-    this.trainingList = [{id: 1,name: "MMA"},
-    {id: 2,name: "BOX"},
-    {id: 3,name: "KOX"},
-    {id: 4,name: "LOS"},
-    {id: 5,name: "RNG"}
-    ]
+    // this.trainingList = [{id: 1,name: "MMA"},
+    // {id: 2,name: "BOX"},
+    // {id: 3,name: "KOX"},
+    // {id: 4,name: "LOS"},
+    // {id: 5,name: "RNG"}
+    // ]
   }
 
   ngOnInit(): void {
+    // TODO: user Service
+    // this.userSub =
+    //   this.userService.getAuthenticationEvent().subscribe(user => {
+    //       this.user = user;
+    //       if (this.user != null)
+    //         this.trainingSub = this.trainingService.getTrainingShedule(user.id).subscribe(trainings => this.trainingList = trainings);
+    //     }
+    //   );
   }
 
   entryRegistration(): void{
-    this.cosiek=true 
+    this.alertnotok.open()
+
   }
 }
