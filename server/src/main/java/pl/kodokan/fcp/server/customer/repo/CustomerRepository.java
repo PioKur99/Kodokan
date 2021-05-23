fbeed9b4449f821a58f4ec2277103268578709d7
grzetan <grzetan@gmail.com>
2021-05-23 14:31:36 +0200

    Debugged main query

--- server/src/main/java/pl/kodokan/fcp/server/customer/repo/CustomerRepository.java
+++ server/src/main/java/pl/kodokan/fcp/server/customer/repo/CustomerRepository.java
@@ -14,7 +14,7 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     List<Customer> findAllByClubCard_Id(Long cardID);
 
-    @Query("SELECT new pl.kodokan.fcp.server.customer.dto.FilteredCustomersDTO(u.id,ud.firstName,ud.lastName) FROM Customer u LEFT JOIN UserData ud ON u.userData=ud.id WHERE ud.firstName LIKE %:firstName% AND ud.lastName LIKE %:lastName% AND CAST(u.clubCard.id as text) LIKE %:cardID% AND CAST(ud.phone as text) LIKE %:phone% AND CAST(u.clubCard.state as text) LIKE %:cardState%")
+    @Query("SELECT new pl.kodokan.fcp.server.customer.dto.FilteredCustomersDTO(u.id,u.userData.firstName,u.userData.lastName) FROM Customer u WHERE u.userData.firstName LIKE %:firstName% AND u.userData.lastName LIKE %:lastName% AND ISNULL(CAST(u.clubCard.id as text),'') LIKE %:cardID% AND ISNULL(CAST(u.userData.phone as text),'') LIKE %:phone% AND (:cardState = '' OR CAST(u.clubCard.state as text) = :cardState)")
     List<FilteredCustomersDTO> getCustomers(@Param("firstName") String firstName, @Param("lastName") String lastName, @Param("cardID") String cardID, @Param("phone") String phone, @Param("cardState") String cardState);
 
 }
