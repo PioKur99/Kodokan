0570da8fff3bb2578e1c434e8db8280c11f7aa54
Grizzly <44000679+JablonskiKJ@users.noreply.github.com>
2021-05-28 17:25:46 +0200

    .

--- client/src/app/data/card/card-state.ts
+++ client/src/app/data/card/card-state.ts
@@ -2,4 +2,14 @@ export class CardState {
     previous: number
     actual: number
     next: number
+
+    constructor(previous?: number, actual?: number, next?: number){
+        if(!previous && !actual && !next){
+           
+        }else{
+            this.previous=previous
+            this.actual=actual
+            this.next=next
+        }
+    }
 }
--- client/src/app/receptionist-panel/membership-cards/membership-cards.component.ts
+++ client/src/app/receptionist-panel/membership-cards/membership-cards.component.ts
@@ -4,6 +4,7 @@ import { Subscription } from 'rxjs';
 import { Card } from 'src/app/data/card/card';
 import { CardState } from 'src/app/data/card/card-state';
 import { Customer } from 'src/app/data/customer/customer';
+import { CustomerAndCardState } from 'src/app/data/customer/customer-and-card-state';
 import { CustomerService } from 'src/app/services/customer.service';
 
 @Component({
@@ -17,7 +18,7 @@ export class MembershipCardsComponent implements OnInit {
   public caseSensitive = false;
   public exactMatch = false;
 
-  cardState: Card[];
+  cardStates: Card[];
   public selectedCardState: string[]
   
   @ViewChild('grid',{static:true}) public grid: IgxGridComponent
@@ -26,28 +27,50 @@ export class MembershipCardsComponent implements OnInit {
   public customerSub: Subscription
   public cardStateList: CardState[]
   public cardStateSub: Subscription
-
+  
+  public customerAndCardStateList: CustomerAndCardState[] = new Array<CustomerAndCardState>()
+  
   constructor(public customerService: CustomerService) { 
   }
 
   ngOnInit(): void {
-    this.cardState=[{id:1,name: 'Nieopłacona'},
-    {id:2,name: 'Opłacona'},
-    {id:3,name: 'Wydrukowana'},
-    {id:4,name: 'Do odbioru'},
-    {id:5,name: 'odebrana'}]
+    this.cardStates=[
+    {id: 1, name: 'Nieopłacona'},
+    {id: 2, name: 'Opłacona'},
+    {id: 3, name: 'Wydrukowana'},
+    {id: 4, name: 'Do odbioru'},
+    {id: 5, name: 'Odebrana'}]
 
+    this.customerList=[
+    {customer_id: 1, first_name: 'Ali', last_name: 'Muhammad'},
+    {customer_id: 2, first_name: 'Bali', last_name: 'Chill'}]
+
+    //pobranie customerów
     this.customerSub = this.customerService.getCustomers().subscribe(
       x => this.customerList=x
       //TODO: obsłużyć error
     )
 
-    let i:any
-    for(i in this.customerList){
-      this.cardStateSub = this.customerService.getCardState(i.customer_id).subscribe(
-        x=> this.cardStateList.push(x)
-        //TODO: obsłużyć error
-      )
-    }
+    //przepisanie customerów
+    this.customerList.forEach(
+      x =>{
+        this.customerAndCardStateList.push(new CustomerAndCardState(x,new CardState()))
+      }
+    )
+
+    let k = new CardState(1,2,3)
+    this.customerAndCardStateList.forEach(x => x.cardState=k)
+      
+
+    this.customerAndCardStateList.forEach(
+      x => {
+        this.cardStateSub = this.customerService.getCardState(x.customer.customer_id).subscribe(
+          y=>{
+            x.cardState=y
+          }
+          //obsłużyć error
+        )
+      }
+    )
   }
 }
