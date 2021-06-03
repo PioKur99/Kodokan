a0a3764273b56510a6324537804c10c21d58c016
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2021-06-03 14:07:16 +0200

    HTML, CSS done

--- client/src/app/receptionist-panel/receptionist-panel.module.ts
+++ client/src/app/receptionist-panel/receptionist-panel.module.ts
@@ -7,7 +7,15 @@ import {
     IgxButtonModule,
     IgxBottomNavModule,
     IgxIconModule,
-    IgxDialogModule
+    IgxDialogModule,
+    IgxInputGroupModule,
+    IgxToggleModule,
+    IgxDropDownModule,
+    IgxCheckboxModule,
+    IgxGridModule,
+    IgxRadioModule,
+    IgxSelectModule,
+    IgxSwitchModule
 } from 'igniteui-angular';
 import { CustomersComponent } from './customers/customers.component';
 import { EntranceComponent } from './entrance/entrance.component';
@@ -16,7 +24,7 @@ import { PassesComponent } from './passes/passes.component';
 import { AddAClientComponent } from './add-a-client/add-a-client.component';
 import { EntryRegistrationComponent } from './entry-registration/entry-registration.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
     declarations: [
         ReceptionistPanelComponent,
@@ -35,7 +43,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         IgxBottomNavModule,
         IgxIconModule,
         //BrowserAnimationsModule,
-        IgxDialogModule
+        IgxDialogModule,
+        IgxInputGroupModule,
+        IgxToggleModule,
+        IgxDropDownModule,
+        IgxCheckboxModule,
+        IgxGridModule,
+        FormsModule,
+        IgxRadioModule,
+        IgxSelectModule,
+        IgxSwitchModule
     ]
 })
 export class ReceptionistPanelModule {
