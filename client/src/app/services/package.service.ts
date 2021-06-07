5dbd6386cb35810ab613ca0807638881f24da2d2
Grizzly <44000679+JablonskiKJ@users.noreply.github.com>
2021-06-07 21:57:29 +0200

    test

--- client/src/app/services/package.service.ts
+++ client/src/app/services/package.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import * as config from '../../assets/config.json'
+import { Customer } from '../data/customer/customer';
 import { PackageDetails } from '../data/package/package-details';
 
 @Injectable({
@@ -12,6 +13,22 @@ export class PackageService {
   constructor(private http: HttpClient) { }
 
   getPackageDetails(package_id: number): Observable<PackageDetails>{
-    return this.http.get<PackageDetails>(config.address+'package_details/'+package_id)
+    //return this.http.get<PackageDetails>(config.address+'package_details/'+package_id)
+    let customer_list: Customer[] = [{customer_id: 1,
+      first_name: "Alex",
+      last_name: "Bord"},
+      {customer_id: 2,
+        first_name: "Charlote",
+        last_name: "Dwayne"}]
+    let package_details : PackageDetails = {name: "Karnet",
+      prize: "30,43",
+      CustomerList: customer_list,
+      endDate: new Date("7.12.2020"),
+      isPaid: true,
+      purchaseDate: new Date("12.11.2020"),
+      entrance_used: 1,
+      entrance_unused: 3,
+      duration: 13.73}
+    return new Observable(x => { x.next(package_details)})
   }
 }
