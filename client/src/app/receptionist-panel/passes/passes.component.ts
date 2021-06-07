9dacc817065ef10f3efe5f347002bdc9ca3f5fc9
Grizzly <44000679+JablonskiKJ@users.noreply.github.com>
2021-06-07 23:26:34 +0200

    tabela + linki
    Zrobić obsługę errorów + CSS

--- client/src/app/receptionist-panel/passes/passes.component.ts
+++ client/src/app/receptionist-panel/passes/passes.component.ts
@@ -12,18 +12,22 @@ export class PassesComponent implements OnInit {
 
   actionUrl=config.address + "customer/"
   packageDetails: PackageDetails
+  packageId: Number = 1
   constructor(public packageService: PackageService) { }
 
   ngOnInit(): void {
     this.getPackageDetails(1)
   }
 
-  getPackageDetails(package_id: number){
-    this.packageService.getPackageDetails(package_id).subscribe(
+  getPackageDetails(packageId: number){
+    this.packageService.getPackageDetails(packageId).subscribe(
       x=>{this.packageDetails=x
         //TODO: wyłapać error
       }
     )
 
+  }
+  showEntrances(){
+    window.location.href="receptionist-panel/entrance/"+ this.packageId.toString()
   }
 }
