a4affb1977bc85441ba26d5534cfe666c08bb382
Ola <32633868+ola2703@users.noreply.github.com>
2021-06-17 13:06:29 +0200

    Delete unused code.

--- client/src/app/data/client.ts
+++ client/src/app/data/client.ts
@@ -1,5 +1,3 @@
-import {WebcamImage} from 'ngx-webcam';
-
 export class Client {
 
     addressLine: String;
@@ -17,33 +15,3 @@ export class Client {
 
 }
 
-/*{
-    "addressLine": "string",
-    "city": "string",
-    "email": "string",
-    "firstName": "string",
-    "gender": "FEMALE",
-    "identityNumber": "string",
-    "image": "string",
-    "lastName": "string",
-    "password": "string",
-    "phone": "string",
-    "postalCode": "string",
-    "voivodeship": "string"
-  }
-
-  export class Client {
-    email: String;
-    phoneNumber: String;
-    gender: String;
-    name: String;
-    surname: String;
-    pesel: String;
-    street: String;
-    localNumber: String;
-    city: String;
-    postCode: String;
-    province: String;
-    image: WebcamImage;
-    image64: String;
-}*/
