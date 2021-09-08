9a2d06e0a150657c01c88bd07c702347483004e7
Łukasz Ważny <lukawaz977@student.polsl.pl>
2021-09-08 18:47:48 +0000

    timesheet id

--- server/src/main/java/pl/kodokan/fcp/server/employee/model/TimeSheet.java
+++ server/src/main/java/pl/kodokan/fcp/server/employee/model/TimeSheet.java
@@ -12,6 +12,11 @@ import java.time.LocalDateTime;
 @Entity
 @Getter @Setter @NoArgsConstructor
 public class TimeSheet extends BaseEntity {
+    
+    @Getter @Setter
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "timesheet_generator")
+    @SequenceGenerator(name="timesheet_generator", sequenceName = "timesheet_seq", allocationSize=50)
+    private Long id;
 
     @NotNull
     private LocalDateTime startDateTime;
