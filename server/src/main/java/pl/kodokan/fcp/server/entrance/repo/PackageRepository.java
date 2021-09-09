571377ee34d98d7c456e4d966e6864b45bb62f31
Łukasz Ważny <lukawaz977@student.polsl.pl>
2021-09-09 18:04:07 +0000

    conflict resolved

--- server/src/main/java/pl/kodokan/fcp/server/entrance/repo/PackageRepository.java
+++ server/src/main/java/pl/kodokan/fcp/server/entrance/repo/PackageRepository.java
@@ -8,6 +8,9 @@ import pl.kodokan.fcp.server.entrance.model.Package;
 
 @Repository
 public interface PackageRepository extends JpaRepository<Package, Long> {
+    @Query("SELECT p FROM Package p JOIN p.customers c WHERE c.id = :id AND (p.endDateTime > CURRENT_TIMESTAMP OR p.endDateTime = null)")
+    List<Package> findFamilyPackages(@Param("id") Long id);
+
     @Query("SELECT p FROM Package p WHERE p.customer.id=:customerID AND p.endDateTime = null AND p.packageType.entranceLimit > 1")
     Package findPackagesWithNoEndDate(@Param("customerID") Long customerID);
 }
