a437aa86bbca1c0c6384779c35606221396ed069
lukaszwazny <32484689+lukaszwazny@users.noreply.github.com>
2021-09-16 22:18:16 +0200

    add transactional

--- server/src/main/java/pl/kodokan/fcp/server/customer/repo/CustomerRepository.java
+++ server/src/main/java/pl/kodokan/fcp/server/customer/repo/CustomerRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import pl.kodokan.fcp.server.customer.model.Customer;
 import pl.kodokan.fcp.server.customer.model.Family;
 
@@ -19,6 +21,7 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Set<String> findAllEmails();
     List<Customer> findAllByClubCard_Id(Long cardID);
 
+    @Transactional
     @Query("SELECT u FROM Customer u WHERE u.userData.firstName LIKE %:firstName% AND u.userData.lastName LIKE %:lastName% AND COALESCE(CAST(u.clubCard.id as text),'') LIKE %:cardID% AND COALESCE(CAST(u.userData.phone as text),'') LIKE %:phone% AND (:cardState = '' OR CAST(u.clubCard.state as text) = :cardState)")
     List<Customer> getCustomers(@Param("firstName") String firstName, @Param("lastName") String lastName, @Param("cardID") String cardID, @Param("phone") String phone, @Param("cardState") String cardState);
 
