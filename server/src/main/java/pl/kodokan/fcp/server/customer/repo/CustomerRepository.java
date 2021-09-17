25bb079b3c1b74cbdcd161c7b7fe40b19172711b
lukaszwazny <32484689+lukaszwazny@users.noreply.github.com>
2021-09-17 09:16:24 +0200

    fix

--- server/src/main/java/pl/kodokan/fcp/server/customer/repo/CustomerRepository.java
+++ server/src/main/java/pl/kodokan/fcp/server/customer/repo/CustomerRepository.java
@@ -22,7 +22,7 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
     List<Customer> findAllByClubCard_Id(Long cardID);
 
     @Transactional
-    @Query("SELECT u FROM Customer u WHERE LOWER(u.userData.firstName) LIKE %LOWER(:firstName)% AND LOWER(u.userData.lastName) LIKE %LOWER(:lastName)% AND COALESCE(CAST(u.clubCard.id as text),'') LIKE %:cardID% AND COALESCE(CAST(u.userData.phone as text),'') LIKE %:phone% AND (:cardState = '' OR CAST(u.clubCard.state as text) = :cardState)")
+    @Query("SELECT u FROM Customer u WHERE u.userData.firstName LIKE %:firstName% AND u.userData.lastName LIKE %:lastName% AND COALESCE(CAST(u.clubCard.id as text),'') LIKE %:cardID% AND COALESCE(CAST(u.userData.phone as text),'') LIKE %:phone% AND (:cardState = '' OR CAST(u.clubCard.state as text) = :cardState)")
     List<Customer> getCustomers(@Param("firstName") String firstName, @Param("lastName") String lastName, @Param("cardID") String cardID, @Param("phone") String phone, @Param("cardState") String cardState);
 
     @Query("SELECT c FROM Customer c WHERE c.family = :family")
